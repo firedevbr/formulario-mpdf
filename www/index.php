dbb04113cbcffbfea10c9326635df893d2080596
Guilherme Mallmann <guilherme.costa@kinghost.com.br>
2020-06-12 20:22:09 -0300

    Melhorando o layout do formulário

--- www/index.php
+++ www/index.php
@@ -1,19 +1,10 @@
 <?php
 
-require_once 'mpdf/vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
-$mpdf = new \Mpdf\Mpdf();
-$mpdf->WriteHTML('
+$baseTemplate = file_get_contents('formulario.html');
 
-<h1>Ficha Cadastral</h1>
-
-<img style="max-height: 600px;
-max-width: 800px;
-width: 480px;
-height: 480px;" src="face_ficha_clinica.jpg" alt="">
-
-');
-
-
+$mpdf = new \Mpdf\Mpdf(['orientation' => 'L']);
+$mpdf->WriteHTML($baseTemplate);
 
 $mpdf->Output();
